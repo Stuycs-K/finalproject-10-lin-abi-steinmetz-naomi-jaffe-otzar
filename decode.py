17edc7957935e2e91226b998aa45b499e7453cd5
yellowstoneparks <otzarj@nycstudents.net>
2025-05-19 17:50:19 -0400

    finished a non-tested decode

--- decode.py
+++ decode.py
@@ -13,9 +13,11 @@ decoded_bits = []
 # Get an arr of the last bits
 for bit in frame_bytes:
 	last_bit = bit & 1
-	decoded_bits.append(int(last_bit))
+	decoded_bits.append(str(int(last_bit)))
 
-# Group arr by eights
+# Group arr by eights, convert to decimal, then to unicode
 decoded_bytes = []
 for i in range(0, len(decoded_bits) // 8, 8):
-	decoded_bytes.append()
+	decoded_bytes.append(chr(int("".join(decoded_bits[i:i+8]),2)))
+
+print("".join(decoded_bits))
