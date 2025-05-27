dfbff0d3036649ea2783465f898d2a1e71a68c99
yellowstoneparks <otzarj@nycstudents.net>
2025-05-26 23:45:17 -0400

    samples

--- phase/encode.py
+++ phase/encode.py
@@ -1,8 +1,25 @@
 import wave
 import numpy as np
 import sys
+import struct
 
 file = sys.argv[1]
 message = sys.argv[2]
 
+# 1: read file
+audio = wave.open(file, "rb")
+parameters = audio.getparams()
+num_channels = audio.getnchannels()
+sample_width = audio.getsampwidth()
+num_frames = audio.getnframes()
+num_samples = num_frames * num_channels
+audio_frames = audio.readframes(num_frames)
+frame_bytes = bytearray(list(audio_frames)) #convert song to byte array
 
+# 2: get the samples
+samples = []
+for i in range(0, len(frame_bytes), 2):
+	samples.append(int.from_bytes(frame_bytes[i: i+2], byteorder="little", signed=True))
+
+print(num_samples)
+print(len(samples))
