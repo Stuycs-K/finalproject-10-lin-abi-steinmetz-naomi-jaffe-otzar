8a4ccdbfd22dfb00073be3e6fefcfd27fdaba315
Naomi Steinmetz <nsteinmetz50@stuy.edu>
2025-05-30 15:14:20 -0400

    updated encode

--- phase/encode.py
+++ phase/encode.py
@@ -1,8 +1,7 @@
-import wave
 import numpy as np
 import sys
 import struct
-# import math
+import math
 from scipy.io import wavfile
 from scipy.fft import fft, ifft
 from typing import List
@@ -10,23 +9,10 @@ from typing import List
 file = sys.argv[1]
 message = sys.argv[2]
 
-# 1: read file
 def read_file(file: str) -> List[int, np.arr]:
 	return rate, data = wavefile.read(file)
 
-# 2: get the samples
-def get_samples():
-	samples = []
-	for i in range(0, len(frame_bytes), 2):
-		samples.append(int.from_bytes(frame_bytes[i: i+2], byteorder="little", signed=True))
-
-	print(num_samples)
-	print(len(samples))
-
-	return samples
-
-# 3: message bits
-def get_bits() -> List[int]:
+def get_bits(message: str) -> List[int]:
 	message_bits = []
 	for i in message:
 		byte = ord(i)
@@ -39,22 +25,62 @@ def get_bits() -> List[int]:
 samples = np.fft.fft(samples)
 
 for i in range(num_samples):
-	pass	
+	pass
 
-samples = np.fft.ifft(samples)
+rate, data = wavefile.read(file)
+num_samples = data.shape[0]
+message_bits = get_bits(message)
+message_len = len(message_bits)
 
-# must find chunksize: example uses np code  
-# "The expression 2 * 2 ** np.ceil(np.log2(2 * textLength)) calculates the smallest power of 2 that is greater than or equal to 2 * textLength, and then multiplies the result by 2. Here's how it breaks down"
+# Step 1
+sizeOfChunk = math.ceil(num_samples / message_len)
+numChunks = math.ceil(len(data) / sizeOfChunk)
 
-# for i in samples:
-# 	i = abs(fft(i))
-# 	i = np.angle(i) #idk if we can get around the use of np here
+data_chunks = []
+for i in range(0, len(data), sizeOfChunk):
+	endpoint = i + sizeOfChunk if i + sizeOfChunk < len(data) else len(data)
+	data_chunks.append(data[i:endpoint])
 
-# diff_samples = [samples[i+1] - samples[i] for i range(len(arr) - 1)] 
+# Step 2
+info = []
+for chunk in data_chunks:
+	mag = abs(np.fft.ftt(chunk))
+	angle = np.angle(np.fft.fft(chunk))
+	info.append((mag, angle))
 
-# converting message to phase diffs
-# message_bits[message_bits == 0] = -1
-# message_bits *= -math.pi/2
+# Step 3
+phase_differences = []
+for i in range(1, len(info)):
+	phase_differences.append(info[i][1] - info[i-1][1])
 
-# phase conversion
-# 
+# Step 4
+phi_list = []
+for i in range(len(info)):
+	message_bit = message_bits[i]
+	if message_bit == 0:
+		phi_list.append(np.pi / 2)
+	else:
+		phi_list.append(-np.pi / 2)
+
+# Step 5a SOMEONE PLEASE CHECK EVERY STEP FROM THIS ONE ON (；人；)
+for i in range(message_len):
+	info[sizeOfChunk/2 - message_len + i][1] = phi_list[i]
+
+# Step 5b
+for i in range(message_len):
+	info[sizeOfChunk/2 + 1 + i][1] = phi_list[message_len + 1 - i]
+
+# Step 6
+for i in range(2, numChunks):
+	info[i][1] = info[i-1][1] + phase_differences[i]
+
+# Step 7
+# wth does Aiexp(jphasei) mean????
+# put the thing back into chunks, reconstruct signal
+chunks = np.fft.ifft(chunks)
+channels = []
+for chunk in chunks:
+	channels.extend(chunk)
+
+# Step 8
+wavefile.write("steg", rate, channels)
