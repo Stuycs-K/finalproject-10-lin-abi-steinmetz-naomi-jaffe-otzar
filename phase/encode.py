453cf2a569048028db5ec5b6fea51496d704b632
yellowstonepark <otzarjaffe@gmail.com>
2025-06-01 19:50:18 -0400

    cleaning up code

--- phase/encode.py
+++ phase/encode.py
@@ -3,7 +3,6 @@ import sys
 import struct
 import math
 from scipy.io import wavfile
-from scipy.fft import fft, ifft
 from typing import Tuple, List
 from numpy.typing import NDArray
 
@@ -24,59 +23,74 @@ def get_bits(message: str) -> List[int]:
 	return message_bits
 
 rate, data = read_file(file)
-num_samples = data.shape[0]
-message_bits = get_bits(message)
-message_len = len(message_bits)
 
-# Step 1
-sizeOfChunk = math.ceil(num_samples / message_len)
-numChunks = math.ceil(len(data) / sizeOfChunk)
+def encode_phase(rate: int, data: NDArray[np.float64], message: str) -> None:
+	# determine the type of data (mono or stereo)
+	type_of_data = "mono" if len(data.shape) == 1 else "stereo"
 
-data_chunks = []
-for i in range(0, len(data), sizeOfChunk):
-	endpoint = i + sizeOfChunk if i + sizeOfChunk < len(data) else len(data)
-	data_chunks.append(data[i:endpoint])
+	if type_of_data == "mono":
+		num_samples = data.shape[0]
+		message_bits = get_bits(message)
+		message_len = len(message_bits)
 
-# Step 2
-info = []
-for chunk in data_chunks:
-	mag = abs(np.fft.fft(chunk))
-	angle = np.angle(np.fft.fft(chunk))
-	info.append((mag, angle))
+		# Step 1
+		sizeOfChunk = math.ceil(num_samples / message_len)
+		numChunks = math.ceil(len(data) / sizeOfChunk)
 
-# Step 3
-phase_differences = []
-for i in range(1, len(info)):
-	phase_differences.append(info[i][1] - info[i-1][1])
+		data_chunks = []
+		for i in range(0, len(data), sizeOfChunk):
+			endpoint = i + sizeOfChunk if i + sizeOfChunk < len(data) else len(data)
+			data_chunks.append(data[i:endpoint])
 
-# Step 4
-phi_list = []
-for i in range(len(info)):
-	message_bit = message_bits[i]
-	if message_bit == 0:
-		phi_list.append(np.pi / 2)
+		# Step 2
+		info = []
+		for chunk in data_chunks:
+			mag = abs(np.fft.fft(chunk))
+			angle = np.angle(np.fft.fft(chunk))
+			info.append((mag, angle))
+
+		# Step 3
+		phase_differences = []
+		for i in range(1, len(info)):
+			phase_differences.append(info[i][1] - info[i-1][1])
+
+		# Step 4
+		phi_list = []
+		for i in range(len(info)):
+			message_bit = message_bits[i]
+			if message_bit == 0:
+				phi_list.append(np.pi / 2)
+			else:
+				phi_list.append(-np.pi / 2)
+
+		# Step 5a SOMEONE PLEASE CHECK EVERY STEP FROM THIS ONE ON (；人；)
+		for i in range(message_len):
+			info[sizeOfChunk/2 - message_len + i][1] = phi_list[i]
+
+		# Step 5b
+		for i in range(message_len):
+			info[sizeOfChunk/2 + 1 + i][1] = phi_list[message_len + 1 - i]
+
+		# Step 6
+		for i in range(2, numChunks):
+			info[i][1] = info[i-1][1] + phase_differences[i]
+
+		chunks = []
+
+		# Step 7
+		# wth does Aiexp(jphasei) mean????
+		# put the thing back into chunks, reconstruct signal
+		chunks = np.fft.ifft(chunks)
+		channels = []
+		for chunk in chunks:
+			channels.extend(chunk)
+
+		# Step 8
+		wavefile.write("steg", rate, channels)
 	else:
-		phi_list.append(-np.pi / 2)
+		print("stereo audio not supported yet")
 
-# Step 5a SOMEONE PLEASE CHECK EVERY STEP FROM THIS ONE ON (；人；)
-for i in range(message_len):
-	info[sizeOfChunk/2 - message_len + i][1] = phi_list[i]
-
-# Step 5b
-for i in range(message_len):
-	info[sizeOfChunk/2 + 1 + i][1] = phi_list[message_len + 1 - i]
-
-# Step 6
-for i in range(2, numChunks):
-	info[i][1] = info[i-1][1] + phase_differences[i]
-
-# Step 7
-# wth does Aiexp(jphasei) mean????
-# put the thing back into chunks, reconstruct signal
-chunks = np.fft.ifft(chunks)
-channels = []
-for chunk in chunks:
-	channels.extend(chunk)
-
-# Step 8
-wavefile.write("steg", rate, channels)
+if __name__ == "__main__":
+	print("encoding message:", message)
+	encode_phase(rate, data, message)
+	print("done")
